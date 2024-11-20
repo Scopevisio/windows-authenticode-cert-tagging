2e3bb879c9313d8eaaedddf2d184b0b492735e00
Tobias Gesellchen <tobias.gesellchen@scopevisio.com>
2024-11-20 21:26:29 +0100

    Remove unused code

--- third_party/lzma/setup_env.bat
+++
@@ -1,8 +0,0 @@
-:: This script must not rely on any external tools or PATH values.
-@echo OFF
-
-:: Let advanced users checkout the tools in just one P4 enlistment
-if "%SETUP_ENV_LZMA%"=="done" goto :EOF
-set  SETUP_ENV_LZMA=done
-
-set INCLUDE=%INCLUDE%;%~dp0files\C\include
